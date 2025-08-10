23d0e8fb8cd1ac757dd0fc3f2a46e4ce572461e4
Marco Kurzynski <marcokurzynski@icloud.com>
2025-08-10 15:33:05 -0400

    can draw a box with text

--- src/main.c
+++ src/main.c
@@ -1,18 +1,39 @@
+#include <locale.h>
 #include <notcurses/notcurses.h>
 #include <stdio.h>
-#include <assert.h>
+#include <unistd.h>
 
 int main() {
+  setlocale(LC_ALL, "");
   const notcurses_options opts = {
       .termtype = NULL,
-      .loglevel = NCLOGLEVEL_WARNING,
+      .loglevel = NCLOGLEVEL_ERROR,
       .margin_t = 0,
       .margin_r = 0,
       .margin_b = 0,
       .margin_l = 0,
       .flags = 0,
   };
-  struct notcurses *state = notcurses_init(&opts, NULL);
-  assert(state != NULL);
-  return 0;
+  struct notcurses *nc = notcurses_core_init(&opts, NULL);
+  if (nc == NULL) {
+    return EXIT_FAILURE;
+  }
+  unsigned dimy, dimx;
+  struct ncplane *n = notcurses_stddim_yx(nc, &dimy, &dimx);
+
+  nccell ul = {}, ll = {}, lr = {}, ur = {}, hl = {}, vl = {};
+  nccells_rounded_box(n, 0, 0, &ul, &ur, &ll, &lr, &hl, &vl);
+  nccells_rounded_box(n, 0, 0, &ul, &ur, &ll, &lr, &hl, &vl);
+
+  ncplane_box(n, &ul, &ur, &ll, &lr, &hl, &vl, dimy - 1, dimx - 1, 0);
+  const char *title = "Dogan";
+  ncplane_printf_yx(n, dimy / 2 - 1, dimx / 2 - strlen(title) / 2, "Dogan");
+
+  if (notcurses_render(nc)) {
+    notcurses_stop(nc);
+    return EXIT_FAILURE;
+  }
+  notcurses_get_blocking(nc, NULL);
+  notcurses_stop(nc);
+  return EXIT_SUCCESS;
 }
