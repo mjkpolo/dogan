975330ff95531fca31aaac1a88edc11b4bf3f802
Marco Kurzynski <marcokurzynski@icloud.com>
2025-08-10 19:22:34 -0400

    start drawing tiles

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,9 @@ class Dogan {
 public:
   Dogan(ncpp::NotCurses &nc, std::atomic_bool &gameover)
       : nc_(nc), stdplane_(nc_.get_stdplane()), gameover_(gameover),
-        msdelay_(1000) {}
+        msdelay_(1000) {
+    DrawBoard();
+  }
 
   void Ticker() { // FIXME ideally this would be called from constructor :/
     std::chrono::milliseconds ms;
@@ -22,13 +24,71 @@ public:
       mtx_.unlock();
       std::this_thread::sleep_for(ms);
       ncmtx.lock();
-      // if (MoveDown()) {
-      //   gameover_ = true;
-      //   ncmtx.unlock();
-      //   return;
-      // }
       ncmtx.unlock();
     } while (!gameover_);
+  }
+
+  void DrawBoard() {
+    static constexpr auto BOARD_WIDTH = 40;
+    static constexpr auto BOARD_HEIGHT = 30;
+    unsigned y, x;
+    stdplane_->get_dim(&y, &x);
+    board_top_y_ = y - (BOARD_HEIGHT + 2);
+    board_ = std::make_unique<ncpp::Plane>(
+        BOARD_HEIGHT, BOARD_WIDTH * 2, board_top_y_, x / 2 - (BOARD_WIDTH + 1));
+    uint64_t channels = 0;
+    ncchannels_set_fg_rgb(&channels, 0x00b040);
+    ncchannels_set_bg_alpha(&channels, NCALPHA_TRANSPARENT);
+    board_->double_box(0, channels, BOARD_HEIGHT - 1, BOARD_WIDTH * 2 - 1,
+                       NCBOXMASK_TOP);
+    ncchannels_set_fg_alpha(&channels, NCALPHA_TRANSPARENT);
+    board_->set_base("", 0, channels);
+    board_->printf(0, BOARD_WIDTH - strlen("DOGAN") / 2, "DOGAN");
+    nc_.render();
+    int tmpx = x / 2;
+    int tmpy = y / 2;
+    DrawTile(0x00b040, tmpx-12, tmpy);
+    DrawTile(0xb04000, tmpx-12+6, tmpy+2);
+    DrawTile(0x0400b0, tmpx-12+6, tmpy-2);
+    DrawTile(0x00b040, tmpx, tmpy);
+    DrawTile(0xb04000, tmpx+6, tmpy+2);
+    DrawTile(0x0400b0, tmpx+6, tmpy-2);
+  }
+
+  void DrawTile(unsigned int rgb, int x, int y) {
+    const size_t cols = 7;
+    const size_t rows = 4;
+    const char *texture = " a***b "
+                          "a**9**b"
+                          "c*****d"
+                          " c***d ";
+    auto tile = std::make_unique<ncpp::Plane>(rows, cols, y, x, nullptr);
+    uint64_t channels = 0;
+    ncchannels_set_bg_alpha(&channels, NCALPHA_TRANSPARENT);
+    ncchannels_set_fg_alpha(&channels, NCALPHA_TRANSPARENT);
+    tile->set_fg_rgb(rgb);
+    tile->set_bg_alpha(NCALPHA_TRANSPARENT);
+    tile->set_base("", 0, channels);
+    y = 0;
+    x = 0;
+    for (size_t i = 0; i < strlen(texture); i++) {
+      if (texture[i] == '*') {
+        tile->putstr(y, x, "█");
+      } else if (texture[i] == 'a') {
+        tile->putstr(y, x, "▟");
+      } else if (texture[i] == 'b') {
+        tile->putstr(y, x, "▙");
+      } else if (texture[i] == 'c') {
+        tile->putstr(y, x, "▜");
+      } else if (texture[i] == 'd') {
+        tile->putstr(y, x, "▛");
+      } else if (texture[i] != ' ') {
+        tile->putc(y, x, texture[i]);
+      }
+      y += ((x = ((x + 1) % cols)) == 0);
+    }
+    nc_.render();
+    tiles_.push_back(std::move(tile));
   }
 
 private:
@@ -36,9 +96,11 @@ private:
   uint64_t score_;
   std::mutex mtx_; // guards msdelay_
   std::unique_ptr<ncpp::Plane> board_;
+  std::vector<std::unique_ptr<ncpp::Plane>> tiles_;
   ncpp::Plane *stdplane_;
   std::atomic_bool &gameover_;
   std::chrono::milliseconds msdelay_;
+  int board_top_y_;
 };
 
 bool IOLoop(ncpp::NotCurses &nc, Dogan &t, std::atomic_bool &gameover) {
